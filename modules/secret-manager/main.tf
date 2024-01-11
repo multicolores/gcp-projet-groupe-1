c1b4e1e28a686cca438c17455a704952a91d1355
multicolores <florian.tellier02@gmail.com>
2024-01-11 16:00:35 +0100

    feat: Create secret-manager module and add import it in main.tf

---
+++ modules/secret-manager/main.tf
@@ -0,0 +1,17 @@
+resource "google_secret_manager_secret" "secret-manager" {
+  secret_id = "secret-manager-${var.secret-manager-name}"
+
+  labels = {
+    label = "connexion-string-secret-for-cloud-sql-db"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+# TODO - update "my-connexion-string-for-cloud-sql-db"" with password of user used to log in the cloud sql db. 
+resource "google_secret_manager_secret_version" "secret_version" {
+  secret       = google_secret_manager_secret.secret-manager.id
+  secret_data  = "my-connexion-string-for-cloud-sql-db"
+}
