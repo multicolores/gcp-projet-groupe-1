d560aa4fe8ad16695a0e14354961e6b9dbb4527f
multicolores <florian.tellier02@gmail.com>
2024-01-12 14:16:21 +0100

    feat: update IAM module and fix buckets to use unique id

--- modules/IAM/main.tf
+++ modules/IAM/main.tf
@@ -1,24 +1,11 @@
-resource "google_service_account" "postgres_DB" {
-  account_id   = "postgres_DB-service-account"
+resource "google_service_account" "postgres_db" {
+  account_id   = "postgres-db-service-account"
   display_name = "postgres_DB Service Account"
 }
 
 resource "google_project_iam_member" "member" {
   project = "analog-bot-410808"
   role    = "roles/editor"
-  member  = "serviceAccount:${google_service_account.postgres_DB.email}"
+  member  = "serviceAccount:${google_service_account.postgres_db.email}"
 }
 
-resource "google_project_iam_policy" "postgres_DB_policy" {
-  project = "analog-bot-410808"
-  policy_data = <<EOF
-{
-  "bindings": [
-    {
-      "members": ["serviceAccount:${google_service_account.postgres_DB.email}"],
-      "role": "roles/editor"
-    }
-  ]
-}
-EOF
-}
--- modules/IAM/output.tf
+++ modules/IAM/output.tf
@@ -1,8 +1,3 @@
-output "vm-name" {
-  value = resource.google_compute_instance.moninstance1.name
-  description = "The VM name"
-}
-
-output "instance-id" {
-    value = resource.google_compute_instance.moninstance1.id
+output "secret_key_iam" {
+  value = google_service_account.postgres_db.unique_id
 }
